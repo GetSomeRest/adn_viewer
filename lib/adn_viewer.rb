bb9e310b86f54cc8ba90561488987b0d1695569f
prathamalag1994 <prathamalag@berkeley.edu>
2015-07-28 16:04:13 -0700

    bucket checking complete

--- lib/adn_viewer.rb
+++ lib/adn_viewer.rb
@@ -1,12 +1,16 @@
 class Adn_Viewer
-	
+
 	def self.token(key, secret)
 		JSON.parse(CurbFu.post({:host => 'developer.api.autodesk.com', :path => '/authentication/v1/authenticate', :protocol => "https"}, { :client_id => key, :client_secret => secret, :grant_type => 'client_credentials' }).body)
 	end
 
 	def self.create_bucket(token, name, policy)
 		puts "trying"
-		JSON.parse(CurbFu.post({:host => 'developer.api.autodesk.com', :path => '/oss/v1/buckets', :protocol => "https", :header => { "Authorization" => ("Bearer " + token) }}, { :bucketKey => name, :policy => policy }).body)
+		JSON.parse(CurbFu.post({:host => 'developer.api.autodesk.com', :path => '/oss/v1/buckets', :protocol => "https", :headers => { "Authorization" => "Bearer " + token, "Content-Type" => "application/json" }}, { :bucketKey => name, :policy => policy }).messages)
+	end
+
+	def self.check_bucket(token, name)
+		JSON.parse(CurbFu.get({:host => 'developer.api.autodesk.com', :path => '/oss/v1/buckets/' + name + '/details', :protocol => "https", :headers => { "Authorization" => "Bearer " + token, "Content-Type" => "application/json" }}).body)
 	end
 
 end
